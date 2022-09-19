1c62b328c78f2e880a5e76ce40b7edbf1d427071
Vito <vito_bjj@mail.ru>
2022-09-19 14:55:29 +0300

    Create the first version of the program

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        
+        int ticetPrice = 500; // Цена билета
+        int bonusMilePrise = 20; // Стоимость одной бонусной мили
+
+        int bonusMiles = ticetPrice / bonusMilePrise; // Кол-во начисленных бонусных миль
+
+        System.out.println(("Количество начисленных миль:_") + (bonusMiles));
 
     }
 }
